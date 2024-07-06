0bcf1caba1eb4995d095786baabe4a2aadd89655
KR0WLEY <50808176+KR0WLEY@users.noreply.github.com>
2024-07-05 22:26:33 -0300

    Update main.mjs

--- main.mjs
+++ main.mjs
@@ -1,12 +1,13 @@
 import { app, BrowserWindow, Tray, Menu } from 'electron';
-import { takeScreenshot, winTray } from './src/functions.mjs';
+import { takeScreenshot, winTray, Path } from './src/functions.mjs';
+import path from 'path';
 global.mainWindow = null;
 function createWindow () {
     mainWindow = new BrowserWindow({
         width: 465,
         height: 168,
         backgroundColor: '#333333',
-        icon: './assets/imgu.png',
+        icon: path.join(Path, 'assets', 'icon.ico'),
         fullscreenable: false,
         resizable: false,
         autoHideMenuBar: true,
@@ -21,8 +22,8 @@ function createWindow () {
     });
     takeScreenshot();
     mainWindow.loadFile('./src/index.html');
-    mainWindow.on('minimize', () => { mainWindow.hide(); winTray(mainWindow, app, Tray, Menu) });
-    mainWindow.on('close', (e) => { e.preventDefault(); mainWindow.hide() });
+    mainWindow.on('minimize', () => { mainWindow.hide() });
+    mainWindow.on('close', (e) => { e.preventDefault(); mainWindow.hide(); });
 }
 app.whenReady().then(() => {
     createWindow();
